fab0c028896975995686716a025575d01d68fe05
Romain-Constant <romain.constant59@gmail.com>
2023-04-26 10:26:12 +0200

    responsive home page done

--- frontend/src/components/nextLaunch/nextLaunchMobile/NextLaunchMobile.jsx
+++ frontend/src/components/nextLaunch/nextLaunchMobile/NextLaunchMobile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 import styles from "./NextLaunchMobile.module.css";
 import CountdownTimer from "../countdownTimer/CountdownTimer";
 import InfosModal from "../infosModal/InfosModal";
@@ -7,7 +8,7 @@ import InfosModal from "../infosModal/InfosModal";
 function NextLaunchMobile() {
   const url = `https://ll.thespacedevs.com/2.2.0/launch/upcoming/`;
 
-  const [launcherIndex] = useState(0);
+  const [launcherIndex, setLauncherIndex] = useState(0);
   const [data, setData] = useState({});
 
   useEffect(() => {
@@ -22,64 +23,88 @@ function NextLaunchMobile() {
       });
   }, []);
 
-  return (
-    <div className={styles.nextLaunchMobileContainer}>
-      <div className={styles.launcherImageMobileContainer}>
-        {data.results ? (
-          <img
-            className={styles.launcherMobileImage}
-            src={data.results[launcherIndex].image}
-            alt="launcher"
-          />
-        ) : null}
-      </div>
-      <div className={styles.descriptionMobileContainer}>
-        <div className={styles.missionMobileContainer}>
-          <p className={styles.titleMobileDescription}>Mission Name</p>
-          {data.results ? (
-            <h1 className={styles.missionMobileName}>
-              {data.results[launcherIndex].mission.name}
-            </h1>
-          ) : null}
+  const nextLaunch = () => {
+    setLauncherIndex(
+      launcherIndex === data.results.length - 1
+        ? 0
+        : (prevIndex) => prevIndex + 1
+    );
+  };
 
+  const prevLaunch = () => {
+    setLauncherIndex(
+      launcherIndex === 0
+        ? data.results.length - 1
+        : (prevIndex) => prevIndex - 1
+    );
+  };
+
+  return (
+    <div className={styles.launchAndButtonsContainer}>
+      <div className={styles.leftButtonContainer}>
+        <FaAngleLeft className={styles.button} onClick={() => prevLaunch()} />
+      </div>
+      <div className={styles.nextLaunchMobileContainer}>
+        <div className={styles.launcherImageMobileContainer}>
           {data.results ? (
-            <InfosModal
-              missionName={data.results[launcherIndex].mission.name}
-              missionDescription={
-                data.results[launcherIndex].mission.description
-              }
+            <img
+              className={styles.launcherMobileImage}
+              src={data.results[launcherIndex].image}
+              alt="launcher"
             />
           ) : null}
         </div>
-        <div className={styles.rocketMobileContainer}>
-          <p className={styles.titleMobileDescription}>Rocket</p>
-          {data.results ? (
-            <h1 className={styles.missionMobileName}>
-              {data.results[launcherIndex].rocket.configuration.name}
-            </h1>
-          ) : null}
+        <div className={styles.descriptionMobileContainer}>
+          <div className={styles.missionMobileContainer}>
+            <p className={styles.titleMobileDescription}>Mission Name</p>
+            {data.results ? (
+              <h1 className={styles.missionMobileName}>
+                {data.results[launcherIndex].mission.name}
+              </h1>
+            ) : null}
+
+            {data.results ? (
+              <InfosModal
+                missionName={data.results[launcherIndex].mission.name}
+                missionDescription={
+                  data.results[launcherIndex].mission.description
+                }
+              />
+            ) : null}
+          </div>
+          <div className={styles.rocketMobileContainer}>
+            <p className={styles.titleMobileDescription}>Rocket</p>
+            {data.results ? (
+              <h1 className={styles.missionMobileName}>
+                {data.results[launcherIndex].rocket.configuration.name}
+              </h1>
+            ) : null}
+          </div>
+          <div className={styles.padMobileContainer}>
+            <p className={styles.titleMobileDescription}>Pad</p>
+            {data.results ? (
+              <h1 className={styles.missionMobileName}>
+                {data.results[launcherIndex].pad.name}
+              </h1>
+            ) : null}
+          </div>
+          <div className={styles.providerMobileContainer}>
+            <p className={styles.titleMobileDescription}>Service Provider</p>
+            {data.results ? (
+              <h1 className={styles.missionMobileName}>
+                {data.results[launcherIndex].launch_service_provider.name}
+              </h1>
+            ) : null}
+          </div>
         </div>
-        <div className={styles.padMobileContainer}>
-          <p className={styles.titleMobileDescription}>Pad</p>
+        <div className={styles.countdownMobileContainer}>
           {data.results ? (
-            <h1 className={styles.missionMobileName}>
-              {data.results[launcherIndex].pad.name}
-            </h1>
-          ) : null}
-        </div>
-        <div className={styles.providerMobileContainer}>
-          <p className={styles.titleMobileDescription}>Service Provider</p>
-          {data.results ? (
-            <h1 className={styles.missionMobileName}>
-              {data.results[launcherIndex].launch_service_provider.name}
-            </h1>
+            <CountdownTimer data={data} launcherIndex={launcherIndex} />
           ) : null}
         </div>
       </div>
-      <div className={styles.weatherMobileContainer}>
-        {data.results ? (
-          <CountdownTimer data={data} launcherIndex={launcherIndex} />
-        ) : null}
+      <div className={styles.rightButtonContainer}>
+        <FaAngleRight className={styles.button} onClick={() => nextLaunch()} />
       </div>
     </div>
   );
