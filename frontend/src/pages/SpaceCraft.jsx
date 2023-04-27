bd1b30674119c81b62e33da1c2d6f10211228cf0
TessaFondeur <fondeurtessa.plum@gmail.com>
2023-04-27 15:48:59 +0200

    Commit with page SpaceCraft, CSS okay ! Finished.

--- frontend/src/pages/SpaceCraft.jsx
+++ frontend/src/pages/SpaceCraft.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SpaceCraftContainer from "@components/spacecraft-container/SpaceCraftContainer";
+import Article from "../components/article/Article";
 import styles from "./SpaceCraft.module.css";
 
 export default function SpaceCraft() {
   return (
     <div className={styles.spacecraftPage}>
-      <SpaceCraftContainer />
+      <div className={styles.containerGlobal}>
+        <div className={styles.containerLeft}>
+          <SpaceCraftContainer />
+        </div>
+        <div className={styles.containerRight}>
+          <Article />
+        </div>
+      </div>
     </div>
   );
 }
